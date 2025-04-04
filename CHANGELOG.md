e40af1afd58933fdc39a41bb132093d29fa7074f
leopado <leonardo.padovani@padosoft.com>
2025-04-04 11:44:14 +0200

    - ADD Support for PHP Unit 10.0 - ADD Support for PHP Unit 11.0 - REMOVE Support for PHP Unit < 6.5.14

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All Notable changes to `test` will be documented in this file
 
+## 3.0.0 - 2025-04-04
+
+- ADD Support for PHP Unit 10.0
+- ADD Support for PHP Unit 11.0
+- REMOVE Support for PHP Unit < 6.5.14
+
 ## 0.2.2 - 2016-08-28
 
 - CHANGE FileSystemTestable trait constructor now accept a directory base. 
--- src/TestBase.php
+++ src/TestBase.php
@@ -5,22 +5,23 @@ namespace Padosoft\Test;
 use Padosoft\Test\traits\ExceptionTestable;
 use Padosoft\Test\traits\FileSystemTestable;
 use Padosoft\Test\traits\ReflectionTestable;
+use PHPUnit\Framework\TestCase;
 
 /**
  * User: Lore
  * Date: 05/12/2015
  * Time: 12:38
  */
-class TestBase extends \PHPUnit_Framework_TestCase
+class TestBase extends TestCase
 {
     use ReflectionTestable , ExceptionTestable, FileSystemTestable;
 
-    protected function setUp()
+    protected function setUp(): void
     {
 
     }
 
-    protected function tearDown()
+    protected function tearDown(): void
     {
 
     }
